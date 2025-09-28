88c2ddf4ec1f0fdcd120e9f8c25b048c19da7e59
parsa-a11 <atmadyparsa@gmail.com>
2025-09-28 19:10:23 +0330

    Create EquipmentNewsAppParsas

---
+++ src/main/java/com/example/parsasequipmentnewsapp/ParsasEquipmentNewsAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.parsasequipmentnewsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParsasEquipmentNewsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParsasEquipmentNewsAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/parsasequipmentnewsapp/ParsasEquipmentNewsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parsasequipmentnewsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParsasEquipmentNewsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
